32d484f199c9d5cff0e57ce91892220a328ce5ce
Dayeong Yoon <Clairey0323@gmail.com>
2025-08-01 10:10:37 +0900

    [fix] route.ts type 수정

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,9 +1,8 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { S3FileUploadRepository } from '@/backend/infrastructure/repositories/S3FileUploadRepository'
 
-export async function POST(req: NextRequest, { params }: { params: { storage: string } }) {
-    const { fileName, fileType } = await req.json()
-    const storage = params.storage
+export async function POST(req: NextRequest) {
+    const { fileName, fileType, storage } = await req.json()
 
     const fileUploadRepository = new S3FileUploadRepository()
     const url = await fileUploadRepository.generatePresignedUrl(fileName, fileType, storage)
