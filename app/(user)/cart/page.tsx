1a03d8da07e455f7f6f6e5f35ac1180e36914bfd
doyoung <gustmdehdud@gmail.com>
2025-08-04 11:13:54 +0900

    컴포넌트 분리

--- app/(user)/cart/page.tsx
+++ app/(user)/cart/page.tsx
@@ -1,13 +1,11 @@
-'use client';
-
 import Divider from '@/components/common/Divider';
 import styles from './cartPage.module.scss';
 import ChipButton from '@/components/common/ChipButton';
 import Checkbox from '@/components/common/Checkbox';
-import Link from 'next/link';
-import Button from '@/components/common/Button';
 import Flex from '@/components/common/Flex';
 import Text from '@/components/common/Text';
+import CartProduct from '@/components/Cart/CartProduct';
+import PaymentButton from '@/components/Cart/PaymentButton';
 
 const DELIVERY_DESCRIPTION_TEXT = [
   '배송 방법 및 쿠폰/포인트 적용 여부는 결제 시 선택할 수 있습니다.',
@@ -18,7 +16,7 @@ const DELIVERY_DESCRIPTION_TEXT = [
 
 const CartPage = () => {
   return (
-    <article>
+    <article className={styles.cart_wrap}>
       <section className={styles.all_select_bar}>
         <div className={styles.select_input_box}>
           <Checkbox id="allSelect" />
@@ -28,30 +26,9 @@ const CartPage = () => {
       </section>
       <Divider />
       <section>
-        <section className={styles.item_select_bar}>
-          <Checkbox />
-          <ChipButton text="삭제" />
-        </section>
-        <Link href={'/'} className={styles.item_info_wrap}>
-          <span className={styles.item_image}>이미지</span>
-          <div className={styles.item_info}>
-            <h3 className={styles.main_text}>메인 타이틀</h3>
-            <p className={styles.sub_text}>서브타이틀</p>
-            <p className={styles.option_text}>옵션</p>
-          </div>
-        </Link>
-        <div className={styles.item_price}>
-          <p className={styles.price_title}>상품금액</p>
-          <p className={styles.price}>249,000원</p>
-        </div>
-        <div className={styles.delivery_price}>
-          <p className={styles.delivery_title}>배송비</p>
-          <p className={styles.price}>무료</p>
-        </div>
-        <section className={styles.button_wrap}>
-          <Button text="옵션/배송 변경" />
-          <Button text="바로 주문" style="black" />
-        </section>
+        <CartProduct />
+        <Divider />
+        <CartProduct />
         <Divider />
         <section className={styles.select_order_info_wrap}>
           <h2 className={styles.select_order_title}>선택 주문정보</h2>
@@ -99,6 +76,7 @@ const CartPage = () => {
         </section>
         <Divider />
       </section>
+      <PaymentButton />
     </article>
   );
 };
