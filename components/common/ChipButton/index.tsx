004bb69d6b64c3f8cf50ea5121c1f8487a7ebe2d
doyoung <gustmdehdud@gmail.com>
2025-08-04 11:13:35 +0900

    chipButton 스타일 수정

--- components/common/ChipButton/index.tsx
+++ components/common/ChipButton/index.tsx
@@ -5,14 +5,18 @@ import styles from './chipButton.module.scss';
 interface IProps {
   text?: string;
   onClick?: () => void;
-  width?: number;
-  height?: number;
+  minWidth?: number;
+  minHeight?: number;
 }
 
-const ChipButton = ({ text, width = 64, height = 24, onClick }: IProps) => {
+const ChipButton = ({ text, minWidth = 64, minHeight = 24, onClick }: IProps) => {
   return (
     <div className={styles.chip_button_style}>
-      <button className={styles.chip_button} style={{ width: width + 'px', height: height + 'px' }} onClick={onClick}>
+      <button
+        className={styles.chip_button}
+        style={{ minWidth: minWidth + 'px', minHeight: minHeight + 'px' }}
+        onClick={onClick}
+      >
         {text}
       </button>
     </div>
