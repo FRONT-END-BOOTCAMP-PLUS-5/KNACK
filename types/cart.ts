008a98649bb485a6edbadd2fca7ebb23443e35ce
doyoung <gustmdehdud@gmail.com>
2025-08-05 15:13:24 +0900

    cart, brand, category, product 타입 추가

--- types/cart.ts
+++ types/cart.ts
@@ -1,6 +1,18 @@
+import { IProduct, IProductOptionMapping } from './product';
+
 export interface CartRef {
   userId: string;
   productId: number;
   optionMappingId: number;
   count: number;
+}
+
+export interface ICart {
+  count: number;
+  id: number;
+  createdAt: Date;
+  optionMappingId: number;
+  product: IProduct;
+  productOptionMapping: IProductOptionMapping;
+  userId: string;
 }
---
+++ types/category.ts
@@ -0,0 +1,7 @@
+export interface ICategory {
+  id: number;
+  engName: string;
+  korName: string;
+  isPrivate: boolean;
+  createdAt: Date;
+}
