9d3d19f733c731f665aa56663badb3d7b88b5a08
Dayeong Yoon <Clairey0323@gmail.com>
2025-08-05 16:06:42 +0900

    [fix] constants.ts 지우고 상수 auth.ts로 옮김

--- backend/utils/constants.ts
+++
@@ -1,8 +0,0 @@
-export const STORAGE_PATHS = {
-    USER_PROFILE: 'uploads/user/profile',
-    PRODUCT: {
-        THUMBNAIL: 'uploads/product/thumbnail',
-        SLIDER: 'uploads/product/slider',
-        DETAIL: 'uploads/product/detail',
-    },
-}
--- constraint/auth.ts
+++ constraint/auth.ts
@@ -24,4 +24,14 @@ export const SOCIAL_LOGIN_PROVIDERS = [
     icon: '/google_logo.svg',
     label: '구글 로그인',
   },
-] as const; 
+] as const;
+
+// S3 스토리지
+export const STORAGE_PATHS = {
+  USER_PROFILE: 'uploads/user/profile',
+  PRODUCT: {
+    THUMBNAIL: 'uploads/product/thumbnail',
+    SLIDER: 'uploads/product/slider',
+    DETAIL: 'uploads/product/detail',
+  },
+}
