8ddf8679aa65b1f4d234fabd5491db44e4181344
doyoung <gustmdehdud@gmail.com>
2025-08-07 14:12:12 +0900

    상세페이지 API 연결 로직 추가

--- components/products/ProductTopImage/index.tsx
+++ components/products/ProductTopImage/index.tsx
@@ -2,23 +2,21 @@ import Image from 'next/image';
 import styles from './productTopImage.module.scss';
 import { STORAGE_PATHS } from '@/constraint/auth';
 
-const SLIDER_IMAGE = [
-  'levis_nike_trucker_jacket_lightblue-slide1.webp',
-  'levis_nike_trucker_jacket_lightblue-slide2.webp',
-];
+interface IProps {
+  thumbnailImage: string;
+  sliderImage: string;
+}
 
-const ProductTopImage = () => {
+const ProductTopImage = ({ thumbnailImage, sliderImage }: IProps) => {
+  const sliderImages = sliderImage.split(',');
+
   return (
     <article>
       <div className={styles.image_box}>
-        <Image
-          src={`${STORAGE_PATHS.PRODUCT.THUMBNAIL}/levis_nike_trucker_jacket_lightblue-thumbnail.webp`}
-          fill
-          alt="상품 대표 이미지"
-        />
+        <Image src={`${STORAGE_PATHS.PRODUCT.THUMBNAIL}/${thumbnailImage}`} fill alt="상품 대표 이미지" />
       </div>
-      <article className={styles.slide_iamge_wrap}>
-        {SLIDER_IMAGE?.map((item) => (
+      <article className={styles.slide_image_wrap}>
+        {sliderImages?.map((item) => (
           <span className={styles.slide_image_box} key={item}>
             <Image src={`${STORAGE_PATHS.PRODUCT.SLIDER}/${item}`} width={56} height={56} alt="상품 슬라이드 이미지1" />
           </span>
