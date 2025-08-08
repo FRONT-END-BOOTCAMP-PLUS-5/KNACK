e8e90e8145f1ab4afb319e1347b51508a726e1f5
doyoung <gustmdehdud@gmail.com>
2025-08-08 16:53:30 +0900

    my page reset

--- app/(user)/my/page.tsx
+++ app/(user)/my/page.tsx
@@ -2,44 +2,33 @@
 
 import { useRouter } from 'next/navigation';
 import styles from './myPage.module.scss';
+import Flex from '@/components/common/Flex';
+import Text from '@/components/common/Text';
+import Button from '@/components/common/Button';
 
-export default function MyPage(){
-    const router = useRouter();
-    
-    const handleWithdrawalClick = () => {
-        router.push('/my/withdrawal');
-    };
-    
-    return(
-        <main className={styles.my_page}>
-            <div className={styles.my_content}>
-                <h1 className={styles.my_title}>마이페이지</h1>
-                
-                <div className={styles.menu_section}>
-                    <div className={styles.menu_item}>
-                        <span className={styles.menu_text}>내 정보</span>
-                    </div>
-                    <div className={styles.menu_item}>
-                        <span className={styles.menu_text}>주문 내역</span>
-                    </div>
-                    <div className={styles.menu_item}>
-                        <span className={styles.menu_text}>관심 상품</span>
-                    </div>
-                    <div className={styles.menu_item}>
-                        <span className={styles.menu_text}>설정</span>
-                    </div>
-                    
-                    {/* 회원탈퇴 버튼 */}
-                    <div className={styles.withdrawal_section}>
-                        <button 
-                            className={styles.withdrawal_button}
-                            onClick={handleWithdrawalClick}
-                        >
-                            회원탈퇴
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </main>
-    )
+export default function MyPage() {
+  const router = useRouter();
+
+  const handleWithdrawalClick = () => {
+    router.push('/my/withdrawal');
+  };
+
+  return (
+    <main className={styles.my_page}>
+      <section>
+        <Flex>
+          <span className={styles.profile_image}>프로필</span>
+          <Flex direction="column" width="self">
+            <Text>닉네임</Text>
+            <Text>아이디</Text>
+            <Button style="border" size="medium" text="프로필 관리" />
+          </Flex>
+        </Flex>
+      </section>
+
+      <button className={styles.withdrawal_button} onClick={handleWithdrawalClick}>
+        회원탈퇴
+      </button>
+    </main>
+  );
 }
