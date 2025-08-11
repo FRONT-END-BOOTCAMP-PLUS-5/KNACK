29def5a309308b60c0e187a4e13883ba4f2f480b
doyoung <gustmdehdud@gmail.com>
2025-08-11 14:09:32 +0900

    common Button 속성 추가

--- components/common/Button/index.tsx
+++ components/common/Button/index.tsx
@@ -3,17 +3,19 @@
 import styles from './button.module.scss';
 
 interface IProps {
-  text: string;
+  text?: string;
   style?: 'border' | 'black' | 'orange';
   size?: 'medium' | 'large' | 'self';
+  children?: React.ReactNode;
   className?: string;
   onClick?: () => void;
 }
 
-const Button = ({ text, style = 'border', size = 'medium', className, onClick }: IProps) => {
+const Button = ({ text, children, style = 'border', size = 'medium', className, onClick }: IProps) => {
   return (
     <button className={`${className} ${styles.button_style} ${styles[style]} ${styles[size]}`} onClick={onClick}>
       {text}
+      {children}
     </button>
   );
 };
