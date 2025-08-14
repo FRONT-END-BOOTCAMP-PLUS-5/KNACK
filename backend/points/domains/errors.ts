6027bd3dfb063ac80a16a4ca26de746886aa918b
Dayeong Yoon <Clairey0323@gmail.com>
2025-08-14 14:24:59 +0900

    [fix] 쓸모없는 파일 삭제

--- backend/points/domains/errors.ts
+++
@@ -1,7 +0,0 @@
-export class InsufficientPointsError extends Error {
-    status = 400;
-    constructor(message = 'insufficient_points') {
-        super(message);
-        this.name = 'InsufficientPointsError';
-    }
-}
