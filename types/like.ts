8288b2fa0ae3f9d79575b1b11d8f61c7908ba573
doyoung <gustmdehdud@gmail.com>
2025-08-14 15:01:38 +0900

    브랜드 좋아요 타입 추가

--- types/like.ts
+++ types/like.ts
@@ -17,3 +17,24 @@ export interface IBrandList {
   BrandId: number;
   createdAt?: Date | null;
 }
+
+export interface IBrandLikeList {
+  id: number;
+  brand: Brand;
+}
+
+interface Brand {
+  engName: string;
+  korName: string;
+  logoImage: string;
+  id: number;
+  products: Product[];
+}
+
+interface Product {
+  korName: string;
+  engName: string;
+  id: number;
+  thumbnailImage: string;
+  price: number;
+}
