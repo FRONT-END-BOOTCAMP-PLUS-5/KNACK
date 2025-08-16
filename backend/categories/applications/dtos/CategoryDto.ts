a3cc8a2143fb37e272248f1a3be393bbf3b4d386
soyeon park <psy_sott120@naver.com>
2025-08-17 03:22:40 +0900

    [chore] 사용하지 않는 인터페이스 주석처리

--- backend/categories/applications/dtos/CategoryDto.ts
+++ backend/categories/applications/dtos/CategoryDto.ts
@@ -1,16 +1,16 @@
-export interface CategoryDto {
-  id: number;
-  korName: string;
-  engName: string;
-  subCategories: SubCategoryDto[];
-}
+// export interface CategoryDto {
+//   id: number;
+//   korName: string;
+//   engName: string;
+//   subCategories: SubCategoryDto[];
+// }
 
-export interface SubCategoryDto {
-  id: number;
-  korName: string;
-  engName: string;
-}
+// export interface SubCategoryDto {
+//   id: number;
+//   korName: string;
+//   engName: string;
+// }
 
-export interface CategoryResponseDto {
-  categories: CategoryDto[];
-}
+// export interface CategoryResponseDto {
+//   categories: CategoryDto[];
+// }
