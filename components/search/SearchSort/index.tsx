03a92d66283706b19588e40b45f8a41c30c05e9e
soyeon park <psy_sott120@naver.com>
2025-08-18 14:45:08 +0900

    [feat] 상품리스트 페이지 상품 정렬 로직 추가

--- components/search/SearchSort/index.tsx
+++ components/search/SearchSort/index.tsx
@@ -1,15 +1,49 @@
+'use client';
 import styles from './searchSort.module.scss';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
-import filterArrow from '@/public/icons/filter_arrow.svg';
-import checkCircle from '@/public/icons/check_circle.svg';
 import { ISearchProductListRequest } from '@/types/searchProductList';
 import { PRODUCT_FILTER_SORT } from '@/constraint/product';
+import Flex from '@/components/common/Flex';
+import filterArrow from '@/public/icons/filter_arrow.svg';
+import checkCircle from '@/public/icons/check_circle.svg';
+import checkIcon from '@/public/icons/check.svg';
+import { useRouter, useSearchParams } from 'next/navigation';
 
 interface IProps {
   filterQuery: ISearchProductListRequest;
 }
 
 export default function SearchSort({ filterQuery }: IProps) {
+  const [isOpen, setIsOpen] = useState(false);
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+    return () => {
+      document.body.style.overflow = 'auto';
+    };
+  }, [isOpen]);
+
+  const isActive = (value: string) => {
+    if (filterQuery.sort) {
+      return value === filterQuery.sort;
+    }
+    return value === 'latest';
+  };
+
+  const onClickSort = (value: string) => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set('sort', value);
+    router.push(`/search?${params.toString()}`);
+    setIsOpen(false);
+  };
+
   const sort = PRODUCT_FILTER_SORT.find((item) => item.value === filterQuery.sort);
   return (
     <section className={styles.sort_section}>
@@ -25,11 +59,30 @@ export default function SearchSort({ filterQuery }: IProps) {
           <p>품절제외</p>
         </div>
       </div>
-      <div className={styles.sort_right}>
+      <div className={styles.sort_right} onClick={() => setIsOpen(true)}>
         {sort ? <p>{sort?.name}</p> : <p>최신순</p>}
 
         <Image src={filterArrow} alt={'리스트 정렬 아이콘'} width={16} height={16} />
       </div>
+      {isOpen && (
+        <section className={styles.sort_bottom_sheet}>
+          <ul>
+            {PRODUCT_FILTER_SORT.map((item) => (
+              <li
+                key={item.id}
+                className={isActive(item.value) ? styles.active : ''}
+                onClick={() => onClickSort(item.value)}
+              >
+                <Flex justify="between">
+                  <p>{item.name}</p>
+                  {isActive(item.value) && <Image src={checkIcon} alt={'체크 아이콘'} width={16} height={16} />}
+                </Flex>
+              </li>
+            ))}
+          </ul>
+          <div className={styles.background_black} onClick={() => setIsOpen(false)} />
+        </section>
+      )}
     </section>
   );
 }
