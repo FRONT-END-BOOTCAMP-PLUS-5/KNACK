591fe06300fe26b52858d1ec3303b5551e18a0c6
young <gustmdehdud@gmail.com>
2025-08-20 01:34:22 +0900

    상세 페이지 상단 캐러셀 추가

--- components/products/ProductTopImage/index.tsx
+++ components/products/ProductTopImage/index.tsx
@@ -1,24 +1,52 @@
+'use client';
+
 import Image from 'next/image';
 import styles from './productTopImage.module.scss';
 import { STORAGE_PATHS } from '@/constraint/auth';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Scrollbar } from 'swiper/modules';
+
+import 'swiper/css';
+import 'swiper/css/scrollbar';
 
 interface IProps {
-  thumbnailImage: string;
   sliderImage: string;
 }
 
-const ProductTopImage = ({ thumbnailImage, sliderImage }: IProps) => {
+const ProductTopImage = ({ sliderImage }: IProps) => {
   const sliderImages = sliderImage.split(',');
 
   return (
     <article>
-      <div className={styles.image_box}>
-        <Image src={`${STORAGE_PATHS.PRODUCT.THUMBNAIL}/${thumbnailImage}`} fill alt="상품 대표 이미지" />
-      </div>
+      <Swiper
+        scrollbar={{
+          hide: false,
+          horizontalClass: `${styles.scrollbar}`,
+          dragClass: `${styles.drag}`,
+        }}
+        modules={[Scrollbar]}
+        className="mySwiper"
+      >
+        {sliderImages?.map((item, index) => (
+          <SwiperSlide key={item}>
+            <div className={styles.image_box}>
+              <span className={styles.slide_image_box}>
+                <Image src={`${STORAGE_PATHS.PRODUCT.SLIDER}/${item}`} fill alt={'슬라이드 이미지' + index} />
+              </span>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
       <article className={styles.slide_image_wrap}>
-        {sliderImages?.map((item) => (
+        {sliderImages?.map((item, index) => (
           <span className={styles.slide_image_box} key={item}>
-            <Image src={`${STORAGE_PATHS.PRODUCT.SLIDER}/${item}`} width={56} height={56} alt="상품 슬라이드 이미지1" />
+            <Image
+              src={`${STORAGE_PATHS.PRODUCT.SLIDER}/${item}`}
+              width={56}
+              height={56}
+              alt={'슬라이드 이미지' + index}
+            />
           </span>
         ))}
       </article>
