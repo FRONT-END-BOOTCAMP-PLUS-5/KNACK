0b468afd8f6bf28477a1a3ca0045bfdb792271f2
sonwooheon <b29299461@gmail.com>
2025-08-21 10:18:18 +0900

    [feat] reviewCount 수정, store에서 제거(관련 파일도 제거), reviewCount api 추가

--- components/my/ActionGrid/index.tsx
+++ components/my/ActionGrid/index.tsx
@@ -3,6 +3,7 @@ import Flex from '@/components/common/Flex';
 import Text from '@/components/common/Text';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useUserStore } from '@/store/userStore';
 import { useReviewCount } from '@/hooks/useReviewCount';
 
 import PointIcon from '@/public/icons/my_point.png';
@@ -10,13 +11,14 @@ import ReviewIcon from '@/public/icons/my_review.png';
 import CouponIcon from '@/public/icons/my_coupon.png';
 
 const ActionGrid = () => {
-  const { reviewCount } = useReviewCount();
+  const { user } = useUserStore();
+  const { reviewCount, isLoading } = useReviewCount();
 
   return (
     <article className={styles.grid_box}>
       <Flex direction="column" align="center" gap={8} paddingVertical={16}>
         <Image src={PointIcon} alt="포인트" width={28} height={28} />
-        <Text size={1.2}>{0}P</Text>
+        <Text size={1.2}>{user?.point || 0}P</Text>
       </Flex>
       <Flex direction="column" align="center" gap={8} paddingVertical={16}>
         <Image src={CouponIcon} alt="쿠폰" width={28} height={28} />
@@ -25,8 +27,9 @@ const ActionGrid = () => {
       <Flex direction="column" align="center" gap={8} paddingVertical={16}>
         <Link href="/my/review" className={styles.review_link}>
           <Image src={ReviewIcon} alt="리뷰" width={28} height={28} />
-          {/* reviewCount가 null이면 0, 숫자가 있으면 숫자 표시 */}
-          <Text size={1.2}>리뷰 {reviewCount ?? 0}</Text>
+          <Text size={1.2}>
+            리뷰 {isLoading ? '0' : reviewCount}
+          </Text>
         </Link>
       </Flex>
     </article>
