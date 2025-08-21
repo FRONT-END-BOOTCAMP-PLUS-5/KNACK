e250eb4422d923b376619cdb616063313beaf6fa
soyeon park <psy_sott120@naver.com>
2025-08-21 12:30:43 +0900

    [feat] 검색모달 최근 본 상품 추가

--- components/common/SearchModal/index.tsx
+++ components/common/SearchModal/index.tsx
@@ -5,12 +5,45 @@ import SearchHeader from './SearchHeader';
 import RecentKeywords from './RecentKeywords';
 import RecommendedTags from './RecommendedTags';
 import RecentProducts from './RecentProducts';
+import { productsService } from '@/services/products';
+import { useCallback, useEffect, useState } from 'react';
+import { IRecentProduct } from '@/types/product';
 
 interface IProps {
   handleSearchInputClick: (state: boolean) => void;
 }
 
 export default function SearchModal({ handleSearchInputClick }: IProps) {
+  const { getRecentlyProductList } = productsService;
+  const [recentProducts, setRecentProducts] = useState<IRecentProduct[]>([]);
+
+  const handleGetRecentlyProduct = useCallback(
+    (ids: string[]) => {
+      const params = new URLSearchParams();
+
+      if (!ids) return;
+
+      ids.forEach((id) => params.append('id', id));
+
+      getRecentlyProductList(params.toString())
+        .then((res) => {
+          if (res.status === 200) {
+            setRecentProducts(res.result);
+          }
+        })
+        .catch((error) => {
+          console.log('error', error.message);
+        });
+    },
+    [getRecentlyProductList]
+  );
+
+  useEffect(() => {
+    const storage = localStorage.getItem('recent') && JSON.parse(localStorage.getItem('recent') ?? '');
+
+    handleGetRecentlyProduct(storage);
+  }, [handleGetRecentlyProduct]);
+
   return (
     <article className={styles.search_container}>
       <SearchHeader handleSearchInputClick={handleSearchInputClick} />
@@ -19,7 +52,7 @@ export default function SearchModal({ handleSearchInputClick }: IProps) {
         <RecentKeywords handleSearchInputClick={handleSearchInputClick} />
         <RecommendedTags handleSearchInputClick={handleSearchInputClick} />
         {/* <PopularKeywords /> */}
-        <RecentProducts handleSearchInputClick={handleSearchInputClick} />
+        <RecentProducts recentProducts={recentProducts} handleSearchInputClick={handleSearchInputClick} />
       </section>
     </article>
   );
