e0df15f93d004f2543e9aebdb9868745c302b330
doyoung <gustmdehdud@gmail.com>
2025-08-21 16:42:58 +0900

    메인페이지 작업

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,12 +1,20 @@
-'use client';
-
+import styles from './mainPage.module.scss';
+import Flex from '@/components/common/Flex';
 import BannerSlide from '@/components/main/BannerSlide';
+import MiddleBanner from '@/components/main/MiddleBanner';
+import RecentProducts from '@/components/main/RecentProducts';
+import SearchCategory from '@/components/search/SearchCategory';
 import 'swiper/css';
 
 export default function Home() {
   return (
-    <section>
+    <section className={styles.home_container}>
       <BannerSlide />
+      <Flex paddingVertical={16} justify="center">
+        <SearchCategory />
+      </Flex>
+      <RecentProducts />
+      <MiddleBanner />
     </section>
   );
 }
