9d7d5dd2568c8adb3299c3fc988c09e9a957f3bf
b29299461@gmail.com <b29299461@gmail.com>
2025-08-22 23:04:15 +0900

    [feat] : 리뷰 더보기 상세팝업 추가

--- components/products/ReviewBottomSheet/index.tsx
+++ components/products/ReviewBottomSheet/index.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import Text from '@/components/common/Text';
-import Flex from '@/components/common/Flex';
 import Button from '@/components/common/Button';
 import { useState } from 'react';
 import styles from './reviewBottomSheet.module.scss';
@@ -25,11 +23,12 @@ interface IProps {
 const ReviewBottomSheet = ({ productData }: IProps) => {
   const [isReviewSheetOpen, setIsReviewSheetOpen] = useState(false);
   
+  // hasReviews를 더 명확하게 정의
+  const hasReviews = Boolean(productData._count.reviews && productData._count.reviews > 0);
+  
   const openReviewSheet = () => setIsReviewSheetOpen(true);
   const closeReviewSheet = () => setIsReviewSheetOpen(false);
   
-  const hasReviews = productData._count.reviews && productData._count.reviews > 0;
-
   // 별점 분포 데이터 생성 (5점부터 1점까지)
   const ratingProgressData = [5, 4, 3, 2, 1].map(rating => ({
     rating,
@@ -39,79 +38,92 @@ const ReviewBottomSheet = ({ productData }: IProps) => {
 
   return (
     <>
-      <Button 
-        text="리뷰 더보기" 
-        size="large" 
-        style="border" 
-        onClick={openReviewSheet}
-      />
+      <button className={styles.review_button} onClick={openReviewSheet}>
+        리뷰 더보기
+      </button>
       
       {/* 바텀시트 */}
       {isReviewSheetOpen && (
         <div className={styles.overlay} onClick={closeReviewSheet}>
           <div className={styles.bottom_sheet} onClick={(e) => e.stopPropagation()}>
             
-            {/* 핸들바 */}
-            <div className={styles.handle} />
-            
-            <Text tag="h3" className={styles.title}>
-              리뷰 상세 정보
-            </Text>
+            {/* 헤더 */}
+            <div className={styles.review_header}>
+              <h3 className={styles.title}>
+                리뷰 더보기
+              </h3>
+              <button className={styles.close_button} onClick={closeReviewSheet}>
+                <img src="/icons/close.svg" alt="닫기" />
+              </button>
+            </div>
             
             {/* 별점 분포 표시 */}
-            {hasReviews && (
-              <div className={styles.rating_container}>
-                <Text tag="h4" className={styles.section_title}>
-                  별점 분포
-                </Text>
-                <Flex className={styles.rating_distribution}>
-                  <Text className={styles.average_rating}>
-                    {productData.averageRating || 0}
-                    <Text tag="span" className={styles.star_icon}>★</Text>
-                  </Text>
-                  <Flex className={styles.rating_list}>
-                    {ratingProgressData.map((item) => (
-                      <Flex className={styles.rating_item} key={item.rating}>
-                        <div className={styles.progress_container}>
-                          <div 
-                            className={styles.progress_bar} 
-                            style={{ width: `${item.percent}%` }} 
-                          />
+            {(() => {
+              
+              if (productData._count.reviews > 0) {
+                return (
+                  <div className={styles.rating_container}>
+                    <div className={styles.rating_summary}>
+                      <div className={styles.average_rating}>
+                        {productData.averageRating && productData.averageRating > 0 ? productData.averageRating : ''}
+                        {productData.averageRating && productData.averageRating > 0 && <span className={styles.star_icon}>★</span>}
+                      </div>
+                      <div className={styles.review_count}>
+                        리뷰 {productData._count.reviews}
+                      </div>
+                    </div>
+                    <div className={styles.rating_distribution}>
+                      {ratingProgressData.map((item) => (
+                        <div className={styles.rating_item} key={item.rating}>
+                          <div className={styles.progress_container}>
+                            <div 
+                              className={styles.progress_bar} 
+                              style={{ width: `${item.percent}%` }} 
+                            />
+                          </div>
+                          <span className={styles.rating_number}>
+                            {item.rating}
+                          </span>
                         </div>
-                        <Text className={styles.rating_number}>
-                          {item.rating}
-                        </Text>
-                      </Flex>
-                    ))}
-                  </Flex>
-                </Flex>
-              </div>
-            )}
+                      ))}
+                    </div>
+                  </div>
+                );
+              } else {
+                return (
+                  <div className={styles.no_reviews}>
+                    <div className={styles.no_reviews_text}>
+                      아직 리뷰가 없어요
+                    </div>
+                    <div className={styles.no_reviews_subtext}>
+                      첫 번째 리뷰를 작성해보세요!
+                    </div>
+                  </div>
+                );
+              }
+            })()}
             
             {/* allQuestionAnswers 데이터를 바텀시트에 표시 */}
             {hasReviews && productData.allQuestionAnswers && (
               <div className={styles.question_results}>
-                <Text tag="h4" className={styles.section_title}>
-                  설문지 결과 상세
-                </Text>
                 {Object.entries(productData.allQuestionAnswers).map(([question, answers]) => (
-                  <div key={question}>
-                    <Text tag="h5" className={styles.question_title}>
+                  <div key={question} className={styles.question_block}>
+                    <h4 className={styles.question_title}>
                       {question}
-                    </Text>
+                    </h4>
                     
                     {Object.entries(answers).map(([answer, stats]) => (
-                      <Flex className={styles.answer_item} key={answer}>
-                        <Text className={styles.answer_text}>{answer}</Text>
-                        <Flex className={styles.answer_stats}>
-                          <Text className={styles.answer_count}>
+                      <div className={styles.answer_item} key={answer}>
+                        <span className={styles.answer_text}>{answer}</span>
+                        <div className={styles.answer_stats}>
+                          <span className={styles.answer_count}>
                             {stats.count}명
-                          </Text>
-                          <Text className={styles.answer_percent}>
+                          </span>
+                          <span className={styles.answer_percent}>
                             {stats.percent}%
-                          </Text>
-                        </Flex>
-                      </Flex>
+                          </span>
+                        </div>
+                      </div>
                     ))}
                   </div>
                 ))}
