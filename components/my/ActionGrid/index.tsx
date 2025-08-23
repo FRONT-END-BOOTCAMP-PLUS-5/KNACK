be2e75f3508a51921cdfe325e5390ffc87fb967e
b29299461@gmail.com <b29299461@gmail.com>
2025-08-23 23:51:40 +0900

    /my 불필요 코드 제거

--- components/my/ActionGrid/index.tsx
+++ components/my/ActionGrid/index.tsx
@@ -3,7 +3,6 @@ import Flex from '@/components/common/Flex';
 import Text from '@/components/common/Text';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useUserStore } from '@/store/userStore';
 import { useReviewCount } from '@/hooks/useReviewCount';
 
 import PointIcon from '@/public/icons/my_point.png';
@@ -11,14 +10,13 @@ import ReviewIcon from '@/public/icons/my_review.png';
 import CouponIcon from '@/public/icons/my_coupon.png';
 
 const ActionGrid = () => {
-  const { user } = useUserStore();
   const { reviewCount, isLoading } = useReviewCount();
 
   return (
     <article className={styles.grid_box}>
       <Flex direction="column" align="center" gap={8} paddingVertical={16}>
         <Image src={PointIcon} alt="포인트" width={28} height={28} />
-        <Text size={1.2}>{user?.point || 0}P</Text>
+        <Text size={1.2}>{0}P</Text>
       </Flex>
       <Flex direction="column" align="center" gap={8} paddingVertical={16}>
         <Image src={CouponIcon} alt="쿠폰" width={28} height={28} />
