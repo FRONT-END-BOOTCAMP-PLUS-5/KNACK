c70dd4976ac34ceef3cf4e7373056e5e7e4e96d9
soyeon park <psy_sott120@naver.com>
2025-09-16 23:56:01 +0900

    [design] 이벤트 목록 ui 크림처럼 변경

--- app/(user)/event/page.tsx
+++ app/(user)/event/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import styles from './event.module.scss';
 import { Metadata } from 'next';
 import { createMetaData } from '@/utils/createMetaData';
+import Text from '@/components/common/Text';
 
 export const metadata: Metadata = createMetaData({
   title: 'EVENT | KNACK',
@@ -14,7 +15,25 @@ export default function EventPage() {
     <div className={styles.event_container}>
       <div className={styles.event_content}>
         <Link href="/event/fishhook" className={styles.event_link}>
-          <Image src="/images/event/event_banner.png" alt="이벤트 내용" width={400} height={300} />
+          <figure>
+            <div className={styles.event_image_box}>
+              <Image
+                src="/images/event/event_banner2.png"
+                alt="이벤트 배너"
+                fill
+                quality={100}
+                className={styles.event_image}
+              />
+            </div>
+            <figcaption className={styles.event_image_caption}>
+              <Text tag="h3" size={1.6} weight={700} marginTop={18}>
+                낚시하고 랜덤 포인트 받기
+              </Text>
+              <Text tag="p" size={1.3} color="gray1" marginTop={4}>
+                랜덤 포인트 최대 1,000P
+              </Text>
+            </figcaption>
+          </figure>
         </Link>
       </div>
     </div>
